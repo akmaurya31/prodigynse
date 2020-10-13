dd640b1fa177e30265e162c37bf7726265f2c294
avinashbcf <avinash.bcfinfotech@gmail.com>
2020-10-13 14:45:05 +0530

    supernse

---
+++ app/controllers/customer.controller.js
@@ -0,0 +1,170 @@
+const Customer = require("../models/customer.model.js");
+//var curl = require("../../node_modules/models/curl");
+// Create and Save a new Customer
+exports.create = (req, res) => {
+  // Validate request
+  if (!req.body) {
+    res.status(400).send({
+      message: "Content can not be empty!"
+    });
+  }
+
+  // Create a Customer
+  const customer = new Customer({
+    email: req.body.email,
+    name: req.body.name,
+    active: req.body.active
+  });
+
+  // Save Customer in the database
+  Customer.create(customer, (err, data) => {
+    if (err)
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while creating the Customer."
+      });
+    else res.send(data);
+  });
+};
+
+// Retrieve all Customers from the database.
+exports.findAll = (req, res) => {
+  Customer.getAll((err, data) => {
+    if (err)
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving customers."
+      });
+    else res.send(data);
+  });
+};
+
+// Find a single Customer with a customerId
+exports.findOne = (req, res) => {
+  Customer.findById(req.params.customerId, (err, data) => {
+    if (err) {
+      if (err.kind === "not_found") {
+        res.status(404).send({
+          message: `Not found Customer with id ${req.params.customerId}.`
+        });
+      } else {
+        res.status(500).send({
+          message: "Error retrieving Customer with id " + req.params.customerId
+        });
+      }
+    } else res.send(data);
+  });
+};
+
+exports.findOne1users = (req, res) => {
+  Customer.findByIdusers(req.params.emailId, (err, data) => {
+    if (err) {
+      if (err.kind === "not_found") {
+        res.status(404).send({
+          message: `Not found Customer with id ${req.params.emailId}.`
+        });
+      } else {
+        res.status(500).send({
+          message: "Error retrieving Customer with id " + req.params.emailId
+        });
+      }
+    } else res.send(data);
+  });
+};
+
+// Update a Customer identified by the customerId in the request
+exports.update = (req, res) => {
+  // Validate Request
+  if (!req.body) {
+    res.status(400).send({
+      message: "Content can not be empty!"
+    });
+  }
+
+  console.log(req.body);
+
+  Customer.updateById(
+    req.params.customerId,
+    new Customer(req.body),
+    (err, data) => {
+      if (err) {
+        if (err.kind === "not_found") {
+          res.status(404).send({
+            message: `Not found Customer with id ${req.params.customerId}.`
+          });
+        } else {
+          res.status(500).send({
+            message: "Error updating Customer with id " + req.params.customerId
+          });
+        }
+      } else res.send(data);
+    }
+  );
+};
+
+// Delete a Customer with the specified customerId in the request
+exports.delete = (req, res) => {
+  Customer.remove(req.params.customerId, (err, data) => {
+    if (err) {
+      if (err.kind === "not_found") {
+        res.status(404).send({
+          message: `Not found Customer with id ${req.params.customerId}.`
+        });
+      } else {
+        res.status(500).send({
+          message: "Could not delete Customer with id " + req.params.customerId
+        });
+      }
+    } else res.send({ message: `Customer was deleted successfully!` });
+  });
+};
+
+// Delete all Customers from the database.
+exports.deleteAll = (req, res) => {
+  Customer.removeAll((err, data) => {
+    if (err)
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while removing all customers."
+      });
+    else res.send({ message: `All Customers were deleted successfully!` });
+  });
+};
+
+
+exports.getusers = (req, res) => {
+  
+  Customer.getAllusers((err, data) => {
+    if (err)
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving customers."
+      });
+    else res.send(data);
+  });
+};
+
+
+
+// Retrieve all Customers from the database.
+exports.bankdata = (req, res) => {
+  var url="https://uat.nsenmf.com/NMFIIService/NMFService/Bank?BrokerCode=ARN-21399&Appln_Id=MFS21399&Password=Account@2121";
+  //curl.get(url, options, function(err, response, body) {});
+
+  // curl(url, function(err) {
+  //   console.info(this.status);
+  //   console.info('-----');
+  //   console.info(this.body);
+  //   console.info('-----');
+  //   console.info(this.info('SIZE_DOWNLOAD'));
+  // });
+
+  var data2={mse:"Teskdsl",nseurl:url};
+  // data['url']=url;
+  // data['body']='body';
+  // data['response']='response';
+
+  res.send(data2);
+
+
+};
