dbd7cb84f9f679366b41eb3ffbcbd478368dc3c3
Avinash Maurya <akmaurya31@gmail.com>
2020-10-14 11:34:37 +0530

    Update customer.controller.js

--- app/controllers/customer.controller.js
+++ app/controllers/customer.controller.js
@@ -80,9 +80,13 @@ const text = "some text BEFORE substring1 AFTER " +"some text BEFORE substring2 
  var sse2= substrings.getAll("BEFORE ", " AFTER", text);
       console.log(sse2);
       
-      var datx=data.date_of_birrth;
-      var sse2cc= substrings.getAll("-", datx);
-       console.log(sse2cc);
+      var names = 'Harry,John,Clark,Peter,Rohn,Alice';
+var nameArr = names.split(',');
+console.log(nameArr);
+      
+//       var datx=data.date_of_birrth;
+//       var sse2cc= substrings.getAll("-", datx);
+//        console.log(sse2cc);
       
       
      // var sse=substrings.getAll("-", data.date_of_birrth);
